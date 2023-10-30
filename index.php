ae6d82b2fa2a3692b11ec43410d8ed3e891754eb
IvsonAssuncao <145385727+IvsonAssuncao@users.noreply.github.com>
2023-10-30 17:15:19 -0300

    eee
    teste

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>SaudeConnect</title>
+</head>
+<body>
+
+    <!-- Barra de Navegação -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">IvsTecnologia</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Início</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cadastro</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Cabeçalho -->
+    <header class="bg-primary text-white text-center py-5">
+        <h1>SaudeConnect</h1>
+        <p>Saude em primeiro lugar!</p>
+    </header>
+
+    
+    <section class="container my-5">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="imagem6.jpg" class="img-fluid" alt="Imagem de Destaque">
+            </div>
+            <div class="col-md-6">
+                <h2>Dicas de saúde</h2>
+                <p>Não perca tempo cadastre  sua receita e receba notificações quando estiver disponivel seu medicamento via watssap na palma da sua mão.</p>
+                <a href="#" class="btn btn-primary">Cadastro de medicamento</a>
+            </div>
+        </div>
+    </section>
+
+    
+    <section class="bg-light py-5">
+        <div class="container">
+            <h2 class="text-center">Inscreva-se para receber nossas atualizações</h2>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Seu nome">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Seu e-mail">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Inscrever-se</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Rodapé -->
+    <footer class="bg-dark text-white text-center py-3">
+        <p>&copy; 2023 Minha Empresa. Todos os direitos reservados.</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
